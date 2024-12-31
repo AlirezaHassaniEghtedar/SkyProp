88e56d32f5735e9452a42c9bc86e43404cf9d8a2
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2024-12-31 15:19:33 +0330

    landing completed 01 - 3:19 pm

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,105 @@
-var swiper = new Swiper(".mySwiper", {
+const mobileMenuIcon = document.querySelector('header .right-section .mobile-menu-icon')
+const mobileNavMenu = document.querySelector('header nav')
+const mobileNavMenuCloseBtn = document.querySelector('header nav .close-btn')
+
+
+
+// Event Listeners
+mobileMenuIcon.addEventListener('click' , handleMobileMenuIconClick)
+mobileNavMenuCloseBtn.addEventListener('click' , handleMobileMenuIconClick)
+
+// Functions
+function handleMobileMenuIconClick () {
+  mobileNavMenu.classList.toggle('active')
+}
+
+
+
+
+
+var swiper = new Swiper(".heroSection .mySwiper", {
     cssMode: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".heroSection .swiper-button-next",
+      prevEl: ".heroSection .swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
+      el: ".heroSection .swiper-pagination",
     },
     loop: true,
     mousewheel: true,
     keyboard: true,
+  });
+
+  var swiper = new Swiper(".plans-section .mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    cssMode: true,
+    navigation: {
+      nextEl: ".plans-section .swiper-button-next",
+      prevEl: ".plans-section .swiper-button-prev",
+    },
+    pagination: {
+      el: ".plans-section .swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    mousewheel: true,
+    keyboard: true,
+
+    breakpoints: {
+      570: {
+        slidesPerView : 1 ,
+        spaceBetween : 15,
+      },
+      767: {
+        slidesPerView : 1 ,
+        spaceBetween : 15,
+      },
+      840: {
+        slidesPerView : 2 ,
+        spaceBetween : 20,
+      },
+      1150: {
+        slidesPerView : 3 ,
+        spaceBetween : 25,
+      },
+    }
+  });
+
+
+  var swiper = new Swiper(".videos-section .mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    cssMode: true,
+    navigation: {
+      nextEl: ".videos-section .swiper-button-next",
+      prevEl: ".videos-section .swiper-button-prev",
+    },
+    pagination: {
+      el: ".videos-section .swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    mousewheel: true,
+    keyboard: true,
+
+    breakpoints: {
+      570: {
+        slidesPerView : 1 ,
+        spaceBetween : 15,
+      },
+      767: {
+        slidesPerView : 1 ,
+        spaceBetween : 15,
+      },
+      840: {
+        slidesPerView : 2 ,
+        spaceBetween : 20,
+      },
+      1150: {
+        slidesPerView : 3 ,
+        spaceBetween : 25,
+      },
+    }
   });
